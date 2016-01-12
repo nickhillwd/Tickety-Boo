26807ba1ad50d5ba29a15f2de943366ebe23d614
nickhillwd <nickhillwd@gmail.com>
2016-01-12 18:49:33 +0000

    edit and new venue fields added but not persisting

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,8 +4,34 @@ class VenuesController < ApplicationController
   
   end
 
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    venue = venue.find(params[:id])
+    venue.update(venue_params)
+    redirect_to admin_index_path
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    venue_new = Event.new(venue_params)
+    venue_new.save
+    redirect_to admin_index_path
+  end
+
   def show
     @venue = Venue.find(params[:id])
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:venue_name, :venue_location, :venue_capacity)
   end
 
 end
