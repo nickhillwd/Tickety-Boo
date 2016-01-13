6777d960d1bfa5d6b14c1a4fa948b34e047c8205
nickhillwd <nickhillwd@gmail.com>
2016-01-13 15:01:06 +0000

    collection select for feature change working

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,4 +1,19 @@
 class FeaturesController < ApplicationController
+
   def edit
+    @feature = Feature.find(params[:id])
   end
+
+  def update
+    feature = feature.find(params[:id])
+    feature.update(feature_params)
+    redirect_to admin_index
+  end
+
+  private
+
+  def feature_params
+    params.require(:feature).permit(:event_id)
+  end
+
 end
