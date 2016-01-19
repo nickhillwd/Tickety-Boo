f1d8b2a132fa436b82b946c608d357e017c806da
nickhillwd <nickhillwd@gmail.com>
2016-01-19 13:19:38 +0000

    quantity of tickets creating correct ammount of bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,12 +9,14 @@ class BookingsController < ApplicationController
   end
 
   def create
-
-    new_booking = Booking.new(booking_params)
-    if new_booking.event.sold_out?
-      redirect_to root_path, alert: "Event Sold Out"
+    ticket_quantity = params[:ticket_quantity][0].to_i
+    ticket_quantity.times do
+      new_booking = Booking.new(booking_params)
+      if new_booking.event.sold_out?
+        redirect_to root_path, alert: "Event Sold Out"
+      end
+      new_booking.save
     end
-    new_booking.save
   
     @user = current_user
      redirect_to(root_path)
